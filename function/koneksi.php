9b1c71402df6ed3d334d7861b976b883d59195d6
Fanny Hasbi <fnyhsbi@gmail.com>
2017-11-21 00:24:59 +0700

    Initial commit

---
+++ function/koneksi.php
@@ -0,0 +1,3 @@
+<?php
+
+$koneksi = mysqli_connect("localhost","root","","bandara") OR die(mysql_error());
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Bandar Internasional Ahmad Yani</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+<h1>Bandar Internasional Ahmad Yani</h1>
+
+</body>
+</html>
